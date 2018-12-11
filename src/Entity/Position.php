2a865c6038812aaab3876382c90532976b2e610b
David Pugh <dpugh@podfather.com>
2018-12-11 23:25:06 +0000

    create basic position class

--- src/Entity/Position.php
+++ src/Entity/Position.php
@@ -4,5 +4,29 @@ namespace FPL\Entity;
 
 class Position
 {
+    private $id;
 
+    private $name;
+
+    public function __construct(int $id, string $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
 }
