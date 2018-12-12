4a0e0f8289c760550e8f1c22a9d2ce93ae8e69f9
David Pugh <dpugh@podfather.com>
2018-12-12 16:28:04 +0000

    fixture extends game

--- src/Entity/Fixture.php
+++ src/Entity/Fixture.php
@@ -2,101 +2,10 @@
 
 namespace FPL\Entity;
 
-use DateTime;
-
-class Fixture
+class Fixture extends Game
 {
-    private $id;
-
-    private $kickoff;
-
-    private $gameWeekId;
-
-    private $homeTeamId;
-
-    private $awayTeamId;
-
-    private $finished;
-
     public function __construct(array $data)
     {
-        $this->setId($data['id'])
-            ->setKickoff(new DateTime($data['kickoff_time']))
-            ->setGameWeekId($data['event'])
-            ->setHomeTeamId($data['team_h'])
-            ->setAwayTeamId($data['team_a'])
-            ->setFinished($data['finished']);
-    }
-
-    public function getId():int
-    {
-        return $this->id;
-    }
-
-    public function setId(int $id): self
-    {
-        $this->id = $id;
-
-        return $this;
-    }
-
-    public function getKickoff(): \DateTime
-    {
-        return $this->kickoff;
-    }
-
-    public function setKickoff(\DateTime $kickoff): self
-    {
-        $this->kickoff = $kickoff;
-
-        return $this;
-    }
-
-    public function getGameWeekId(): int
-    {
-        return $this->gameWeekId;
-    }
-
-    public function setGameWeekId(int $gameWeekId): self
-    {
-        $this->gameWeekId = $gameWeekId;
-
-        return $this;
-    }
-
-    public function getHomeTeamId(): int
-    {
-        return $this->homeTeamId;
-    }
-
-    public function setHomeTeamId(int $homeTeamId): self
-    {
-        $this->homeTeamId = $homeTeamId;
-
-        return $this;
-    }
-
-    public function getAwayTeamId(): int
-    {
-        return $this->awayTeamId;
-    }
-
-    public function setAwayTeamId(int $awayTeamId): self
-    {
-        $this->awayTeamId = $awayTeamId;
-
-        return $this;
-    }
-
-    public function isFinished(): bool
-    {
-        return $this->finished;
-    }
-
-    public function setFinished(bool $finished): self
-    {
-        $this->finished = $finished;
-
-        return $this;
+        parent::__construct($data);
     }
 }
