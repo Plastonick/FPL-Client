979aebb051e38a8e33e081fe3a39c9cbd52e6d53
David Pugh <dpugh@podfather.com>
2019-02-12 19:28:50 +0000

    change fixture to be immutable

--- src/Entity/Fixture.php
+++ src/Entity/Fixture.php
@@ -18,12 +18,12 @@ class Fixture
 
     public function __construct(array $data)
     {
-        $this->setId($data['id'])
-            ->setKickoff(new \DateTime($data['kickoff_time']))
-            ->setGameWeekId($data['event'])
-            ->setHomeTeamId($data['team_h'])
-            ->setAwayTeamId($data['team_a'])
-            ->setFinished($data['finished']);
+        $this->id = $data['id'];
+        $this->kickoff = new \DateTime($data['kickoff_time']);
+        $this->gameWeekId = $data['event'] ?? null;
+        $this->homeTeamId = $data['team_h'] ?? null;
+        $this->awayTeamId = $data['team_a'] ?? null;
+        $this->finished = $data['finished'] ?? false;
     }
 
     public function getId():int
@@ -31,70 +31,28 @@ class Fixture
         return $this->id;
     }
 
-    public function setId(int $id): self
-    {
-        $this->id = $id;
-
-        return $this;
-    }
-
     public function getKickoff(): \DateTime
     {
         return $this->kickoff;
     }
 
-    public function setKickoff(\DateTime $kickoff): self
-    {
-        $this->kickoff = $kickoff;
-
-        return $this;
-    }
-
-    public function getGameWeekId(): int
+    public function getGameWeekId(): ?int
     {
         return $this->gameWeekId;
     }
 
-    public function setGameWeekId(int $gameWeekId): self
-    {
-        $this->gameWeekId = $gameWeekId;
-
-        return $this;
-    }
-
-    public function getHomeTeamId(): int
+    public function getHomeTeamId(): ?int
     {
         return $this->homeTeamId;
     }
 
-    public function setHomeTeamId(int $homeTeamId): self
-    {
-        $this->homeTeamId = $homeTeamId;
-
-        return $this;
-    }
-
-    public function getAwayTeamId(): int
+    public function getAwayTeamId(): ?int
     {
         return $this->awayTeamId;
-    }
-
-    public function setAwayTeamId(int $awayTeamId): self
-    {
-        $this->awayTeamId = $awayTeamId;
-
-        return $this;
     }
 
     public function isFinished(): bool
     {
         return $this->finished;
-    }
-
-    public function setFinished(bool $finished): self
-    {
-        $this->finished = $finished;
-
-        return $this;
     }
 }
