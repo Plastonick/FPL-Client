1f58d161f11158b1cc147ab4035ff64c05ff12dc
David Pugh <dpugh@podfather.com>
2019-04-23 19:43:33 +0100

    add example usage file

---
+++ example.php
@@ -0,0 +1,26 @@
+<?php
+
+use Plastonick\FPLClient\Transport\Client;
+
+require_once 'vendor/autoload.php';
+
+// initiate the client
+$client = new Client();
+
+// get a player by ID
+$player = $client->getPlayerById(1);
+
+// get the player's team
+$team = $player->getTeam();
+
+// get upcoming fixtures for a player
+$fixtures = $player->getFixtures();
+
+// get performances by player
+$performances = $player->getPerformances();
+
+// get various stats for a performance
+$points = $performances[0]->getTotalPoints();
+
+// get the fixture for a performance
+$fixture = $performances[0]->getFixture();
